30a109ad73e477e827901ec85bbe2af0f71eb3c2
Mehran Khanjan <mehran@Mehrans-MacBook-Pro.local>
2024-05-03 15:48:08 +0400

    add navbar and about page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import type {AppProps} from "next/app";
+import Header from "@/components/shared/Header";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Header/>
+
+            <div className="container-fluid ">
+
+                <Component {...pageProps} />
+
+            </div>
+        </>
+    );
 }
