c0b30cdfa780ae4d224793c26b303e76142c52d6
Mehran Khanjan <mehrankh1913@gmail.com>
2024-05-08 14:55:28 +0400

    add new content

--- src/pages/blockchain/unit-test-smart-contract.tsx
+++ src/pages/blockchain/unit-test-smart-contract.tsx
@@ -4,9 +4,40 @@ export default function UnitTestSmartContract() {
             <div className="row mt-3">
                 <div className="col-12">
                     <h1>How To Unit Test A Smart Contract</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium animi, blanditiis cupiditate, dicta eligendi eveniet impedit iusto natus nobis ratione repudiandae sapiente tempore veniam voluptas voluptate voluptatum! Facilis, suscipit!</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium animi, blanditiis cupiditate, dicta eligendi eveniet impedit iusto natus nobis ratione repudiandae sapiente tempore veniam voluptas voluptate voluptatum! Facilis, suscipit!</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium animi, blanditiis cupiditate, dicta eligendi eveniet impedit iusto natus nobis ratione repudiandae sapiente tempore veniam voluptas voluptate voluptatum! Facilis, suscipit!</p>
+
+                    <h2>Blockchain Development Series</h2>
+                    <h3>DApp Development</h3>
+                    <h3>Launchpad Development</h3>
+                    <h3>Creating Smart Contract</h3>
+                    <h3>&nbsp;</h3>
+                    <p>I want to create a launchpad DApp platform. The technologies we want to use are:</p>
+                    <ul>
+                        <li>Solidity programming language: to develop smart contract</li>
+                        <li>Hardhat: to manage smart contracts</li>
+                        <li>Chai: to write unit test</li>
+                        <li>Next.js: to develop front-end application</li>
+                        <li>NestJS: to develop back-end application</li>
+                        <li>RabbitMQ: as a message broker for our mircroservices</li>
+                        <li>Docker</li>
+                    </ul>
+                    <p>&nbsp;</p>
+                    <p>The list of our services is:</p>
+                    <ul>
+                        <li>Auth service: to register and authenticate users</li>
+                        <li>Launchpad creation service: to create a new launchpad by user</li>
+                        <li>Launchpad service: let users participate in a launchpad</li>
+                        <li>Worker service: to listen to blockchain transactions</li>
+                    </ul>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium animi, blanditiis
+                        cupiditate, dicta eligendi eveniet impedit iusto natus nobis ratione repudiandae sapiente
+                        tempore veniam voluptas voluptate voluptatum! Facilis, suscipit!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium animi, blanditiis
+                        cupiditate, dicta eligendi eveniet impedit iusto natus nobis ratione repudiandae sapiente
+                        tempore veniam voluptas voluptate voluptatum! Facilis, suscipit!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium animi, blanditiis
+                        cupiditate, dicta eligendi eveniet impedit iusto natus nobis ratione repudiandae sapiente
+                        tempore veniam voluptas voluptate voluptatum! Facilis, suscipit!</p>
                 </div>
             </div>
         </>
