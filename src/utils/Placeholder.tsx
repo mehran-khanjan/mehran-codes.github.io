8b10f27b76303764c3c1868abcdec42d15c7941b
unknown <mehrankh1913@gmail.com>
2025-05-03 12:43:54 +0330

    Add ReactJS course

---
+++ src/utils/Placeholder.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import MarkdownRenderer from "@/components/organisms/MarkdownRenderer";
+import {getMdxContent} from "@/utils/mdxUtils";
+import path from "path";
+
+export async function getStaticProps() {
+    const postPath = path.join(
+        process.cwd(),
+        'src',
+        'pages',
+        '',
+        '',
+        'data.md'
+    );
+    const {fileContent, mdxSource} = await getMdxContent(postPath);
+    return {props: {fileContent, mdxSource}};
+}
+
+type PropsType = {
+    title: string,
+    // fileContent: string,
+    mdxSource: any,
+}
+
+const Placeholder: React.FC<PropsType> = ({mdxSource}) => {
+    return (
+        <React.Fragment>
+
+            <MarkdownRenderer title={''} mdxSource={mdxSource}/>
+
+        </React.Fragment>
+    )
+}
+
+export default Placeholder;
